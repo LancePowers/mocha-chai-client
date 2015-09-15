ce461ff78c8ea1e46db5a7e8cd30da696a9af623
Lance <Lance@thelpstory.com>
2015-09-15 11:04:33 -0600

    all tests pass

--- test/test.js
+++ test/test.js
@@ -1,9 +1,37 @@
 var expect = chai.expect;
-// var should = chai.should();
 
-// sample!
-describe('Compare Numbers', function() {
-  it('1 should equal 1', function() {
-    expect(1).to.equal(1);
+
+describe("Car tests", function () {
+
+  var car = new Car();
+
+  describe("Constructor", function () {
+
+    it("should have zero fuel", function () {
+      expect(car.fuel).to.equal(0);
+    });
+
   });
+
+  describe("Driving tests", function () {
+
+    it("should return an error when attempting to drive without fuel", function () {
+      expect(car.drive()).to.equal('No fuel in car!')
+    });
+
+    it("should set fuel to max when fill is called without argument", function () {
+      car.fill();
+      expect(car.fuel).to.equal(car.max_fuel)
+    });
+
+    it("should set fuel according to driving", function () {
+      expect(car.fuel).to.equal(car.max_fuel)
+    });
+
+    it("should return an error when attempting to drive more than fuel left in car", function () {
+      // test
+    });
+
+  });
+
 });
